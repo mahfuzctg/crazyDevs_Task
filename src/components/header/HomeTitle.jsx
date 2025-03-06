c26ab26c646c73ff3712a734aa6ebef2ee5dd5e7
Mahfuz <aamahfuz.pro@gmail.com>
2025-03-06 16:29:14 +0600

    added home title

---
+++ src/components/header/HomeTitle.jsx
@@ -0,0 +1,21 @@
+const HomeTitle = () => {
+  return (
+    <div className="flex items-center ml-40">
+      <div>
+        <h2>Add Files to</h2>
+      </div>
+      <div>
+        <img
+          src="/src/assets/Home-Head-App-Icon.png"
+          alt="Home-Head-App-Icon"
+          className="w-[70px] h-[70px] rounded-lg"
+        />
+      </div>
+      <div>
+        <h2>Cloud Chat</h2>
+      </div>
+    </div>
+  );
+};
+
+export default HomeTitle;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,20 @@
 import UploadCard from "@/components/card/UploadCard";
 import HomeHeadTitle from "@/components/header/HomeHeadTitle";
+import HomeTitle from "@/components/header/HomeTitle";
 
 const HomePage = () => {
   return (
     <div>
+      {/* Home Head title here */}
       <div>
         <HomeHeadTitle />
       </div>
+      {/* Home title is here */}
+      <>
+        <HomeTitle />
+      </>
+
+      {/* Home upload cards here */}
       <div className="grid grid-cols-2">
         <UploadCard />
       </div>
