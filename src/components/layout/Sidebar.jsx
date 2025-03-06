c37426b7cb0526f9027cc1f0f2943cf01b1a499c
Mahfuz <aamahfuz.pro@gmail.com>
2025-03-06 06:11:26 +0600

    added search icons on sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,15 @@
 const Sidebar = () => {
   return (
     <div className="min-h-screen">
-      {/* Add Image */}
-      <div className="flex gap-2">
-        <img src="/src/assets/Logo-icon.png" alt="Logo-icon" />
-        <img src="/src/assets/Logo-label.png" alt="Website Logo" />
+      <div className="flex">
+        <div className="flex gap-2">
+          <img src="/src/assets/Logo-icon.png" alt="Logo-icon" />
+          <img src="/src/assets/Logo-label.png" alt="Website Logo" />
+        </div>
+        <div className="flex gap-2">
+          <img src="/src/assets/icon-search.png" alt="Logo-icon" />
+          <img src="/src/assets/icon-sidebar-left.png" alt="Website Logo" />
+        </div>
       </div>
     </div>
   );
