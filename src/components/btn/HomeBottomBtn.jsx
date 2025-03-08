a4165d069612d077e465c12ba093a0f62c637903
Mahfuz <aamahfuz.pro@gmail.com>
2025-03-08 15:00:23 +0600

    added deep dive comments

--- src/components/btn/HomeBottomBtn.jsx
+++ src/components/btn/HomeBottomBtn.jsx
@@ -1,31 +1,39 @@
+import DeepDive from "../card/DeepDive";
 import BtnRounded from "../reuseable/BtnRounded";
 
 const HomeBottomBtn = () => {
   return (
-    <div className="flex items-center justify-center gap-6 mt-10   ">
-      {/* Start Button */}
-      <BtnRounded
-        buttonText="Start"
-        className="bg-[#454444] home_bottom_start_btn transition-all duration-200 ease-in-out 
+    <div>
+      <div className="flex items-center justify-center gap-6 mt-10   ">
+        {/* Start Button */}
+        <BtnRounded
+          buttonText="Start"
+          className="bg-[#454444] home_bottom_start_btn transition-all duration-200 ease-in-out 
                    hover:bg-[#FB9937] active:scale-95"
-        border={true}
-        borderColor="border-[#FB9937]"
-        onClick={() => alert("Start Button Clicked!")}
-      />
+          border={true}
+          borderColor="border-[#FB9937]"
+          onClick={() => alert("Start Button Clicked!")}
+        />
 
-      {/* Centered "Or" */}
-      <h3 className="text-white text-lg font-semibold">or</h3>
+        {/* Centered "Or" */}
+        <h3 className="text-white text-lg font-semibold">or</h3>
 
-      {/* Start With Demo Dive Button */}
-      <BtnRounded
-        iconSrc="/assets/icon-deep-dive.png"
-        buttonText="Start With Demo Dive"
-        className="bg-[#FB9937] home_bottom_demo_btn transition-all duration-200 ease-in-out 
+        {/* Start With Demo Dive Button */}
+        <BtnRounded
+          iconSrc="/assets/icon-deep-dive.png"
+          buttonText="Start With Demo Dive"
+          className="bg-[#FB9937] home_bottom_demo_btn transition-all duration-200 ease-in-out 
                    hover:bg-[#454444] active:scale-95"
-        border={true}
-        borderColor="border-[#FB9937]"
-        onClick={() => alert("Demo Dive Button Clicked!")}
-      />
+          border={true}
+          borderColor="border-[#FB9937]"
+          onClick={() => alert("Demo Dive Button Clicked!")}
+        />
+      </div>
+
+      <div className="flex justify-end  -mt-6">
+        {" "}
+        <DeepDive />
+      </div>
     </div>
   );
 };
---
+++ src/components/card/DeepDive.jsx
@@ -0,0 +1,18 @@
+const DeepDive = () => {
+  return (
+    <>
+      <div>
+        <img src="/public/assets/pointing.png" alt="pointing" />
+      </div>
+      <div className="w-[226px] h-[94px] deep_dive_box bg-[#1E1E1E] ">
+        <h6>
+          Deep Dive processes documents section by section for "unlimited"
+          context, enabling complete answers. Run once per chat to unlock Table
+          AI and Prompt Loops
+        </h6>
+      </div>
+    </>
+  );
+};
+
+export default DeepDive;
